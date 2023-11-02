3c23500b3871ea944987602f8e090186ab992ca2
g10z3r <20g10z3r@gmail.com>
2023-11-02 19:48:16 +0100

    Fixed bug if not pointer receiver's in method functions

--- internal/domain/service/anthill/analyzer/function_analyzer.go
+++ internal/domain/service/anthill/analyzer/function_analyzer.go
@@ -2,9 +2,9 @@ package analyzer
 
 import (
 	"context"
+	"errors"
 	"go/ast"
 	"go/token"
-	"log"
 
 	"github.com/g10z3r/archx/internal/domain/service/anthill/obj"
 )
@@ -13,97 +13,60 @@ func NewFuncAnalyzer(file *obj.FileObj) Analyzer[ast.Node, obj.Object] {
 	return NewAnalyzer[ast.Node, obj.Object](
 		file,
 		analyzeFuncNode,
-		checkFuncNode,
 	)
 }
 
-func checkFuncNode(node ast.Node) bool {
-	_, ok := node.(*ast.FuncDecl)
-	return ok
-}
+// func checkFuncNode(node ast.Node) bool {
+// 	_, ok := node.(*ast.FuncDecl)
+// 	return ok
+// }
 
 func analyzeFuncNode(ctx context.Context, f *obj.FileObj, node ast.Node) (obj.Object, error) {
 	funcDecl, _ := node.(*ast.FuncDecl)
 
-	var parentStruct *ast.Ident
-	if funcDecl.Recv != nil {
-		starExpr, ok := funcDecl.Recv.List[0].Type.(*ast.StarExpr)
-		if !ok {
-			return nil, nil // TODO: add error return message
-		}
-
-		parentStruct, ok = starExpr.X.(*ast.Ident)
-		if !ok {
-			return nil, nil // TODO: add error return message
-		}
+	ps, err := getParentStruct(funcDecl)
+	if err != nil {
+		return nil, err
 	}
 
 	params, deps, err := processFuncParams(f.FileSet, funcDecl, f.Entities.Imports.InternalImportsMeta)
 	if err != nil {
-		log.Fatal(err)
+		return nil, errors.New("some error from analyzeFuncNode 3") // TODO: add normal error return message
 	}
 
-	funcObj := obj.NewFuncObj(f.FileSet, funcDecl, params, deps, parentStruct)
+	funcObj := obj.NewFuncObj(f.FileSet, funcDecl, params, deps, ps)
 
 	if err := inspectFuncBody(funcDecl, funcObj, f.Entities.Imports.InternalImportsMeta); err != nil {
-		log.Fatal(err)
+		return nil, errors.New("some error from analyzeFuncNode 4") // TODO: add normal error return message
 	}
 
 	return funcObj, nil
 }
 
-type FunctionAnalyzer struct{}
-
-func (a *FunctionAnalyzer) Name() string {
-	return "func"
-}
-
-func (a *FunctionAnalyzer) Check(node ast.Node) bool {
-	_, ok := node.(*ast.FuncDecl)
-	return ok
-}
-
-func (a *FunctionAnalyzer) Save(f *obj.FileObj, object obj.Object) {
-	funcObj, ok := object.(*obj.FuncObj)
-	if !ok {
-		log.Fatal("not a func objects")
+func getParentStruct(funcDecl *ast.FuncDecl) (*ast.Ident, error) {
+	if funcDecl.Recv == nil || len(funcDecl.Recv.List) == 0 {
+		return nil, nil
 	}
 
-	f.AppendFunc(funcObj)
-}
+	receiverType := funcDecl.Recv.List[0].Type
 
-func (a *FunctionAnalyzer) Analyze(f *obj.FileObj, node ast.Node) obj.Object {
-	funcDecl, _ := node.(*ast.FuncDecl)
-
-	var parentStruct *ast.Ident
-	if funcDecl.Recv != nil {
-		starExpr, ok := funcDecl.Recv.List[0].Type.(*ast.StarExpr)
-		if !ok {
-			return nil
+	switch t := receiverType.(type) {
+	case *ast.StarExpr:
+		// if the receiver's type is a pointer, attempt to get the identifier of the struct
+		if ident, ok := t.X.(*ast.Ident); ok {
+			return ident, nil
 		}
 
-		parentStruct, ok = starExpr.X.(*ast.Ident)
-		if !ok {
-			return nil
-		}
+	case *ast.Ident:
+		// if the receiver's type is not a pointer, it's a regular struct, so return the identifier of the struct
+		return t, nil
 	}
 
-	params, deps, err := processFuncParams(f.FileSet, funcDecl, f.Entities.Imports.InternalImportsMeta)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	funcObj := obj.NewFuncObj(f.FileSet, funcDecl, params, deps, parentStruct)
-
-	if err := inspectFuncBody(funcDecl, funcObj, f.Entities.Imports.InternalImportsMeta); err != nil {
-		log.Fatal(err)
-	}
-
-	return funcObj
+	return nil, errors.New("invalid receiver type in method declaration")
 }
 
 func inspectFuncBody(funcDecl *ast.FuncDecl, funcEntity *obj.FuncObj, impMeta map[string]int) error {
-	// Get the recipient's name if it is a structure method
+	// get the recipient's name if it is a structure method
 	var receiverName string
 	if funcDecl.Recv != nil && len(funcDecl.Recv.List) > 0 {
 		receiverName = funcDecl.Recv.List[0].Names[0].Name
@@ -113,7 +76,7 @@ func inspectFuncBody(funcDecl *ast.FuncDecl, funcEntity *obj.FuncObj, impMeta ma
 		switch expr := n.(type) {
 		case *ast.SelectorExpr:
 			if ident, ok := expr.X.(*ast.Ident); ok {
-				// Structure field call found
+				// structure field call found
 				if ident.Name == receiverName {
 					if usage, exists := funcEntity.Fields[expr.Sel.Name]; !exists {
 						funcEntity.Fields[expr.Sel.Name] = usage
@@ -122,21 +85,21 @@ func inspectFuncBody(funcDecl *ast.FuncDecl, funcEntity *obj.FuncObj, impMeta ma
 					funcEntity.Fields[expr.Sel.Name]++
 				}
 
-				// Found using another internal package
+				// found using another internal package
 				if index, exists := impMeta[ident.Name]; exists {
 					funcEntity.AddDependency(index, expr.Sel.Name)
 				}
 			}
 
 		case *ast.CallExpr:
-			// Check for recursion in regular functions
+			// check for recursion in regular functions
 			if ident, ok := expr.Fun.(*ast.Ident); ok {
 				if ident.Name == funcEntity.Name {
 					funcEntity.Metadata.IsRecursive = true
 				}
 			}
 
-			// Check for recursion in methods
+			// check for recursion in methods
 			if sel, ok := expr.Fun.(*ast.SelectorExpr); ok {
 				if ident, ok := sel.X.(*ast.Ident); ok && ident.Name == receiverName && sel.Sel.Name == funcEntity.Name {
 					funcEntity.Metadata.IsRecursive = true
@@ -149,10 +112,10 @@ func inspectFuncBody(funcDecl *ast.FuncDecl, funcEntity *obj.FuncObj, impMeta ma
 	return nil
 }
 
-type FuncObjParamMap map[string]*obj.FuncObjParam
-type DepObjMap map[string]*obj.EntityDepObj
+type funcObjParamMap map[string]*obj.FuncObjParam
+type depObjMap map[string]*obj.EntityDepObj
 
-func processFuncParams(fset *token.FileSet, funcDecl *ast.FuncDecl, impMeta map[string]int) (FuncObjParamMap, DepObjMap, error) {
+func processFuncParams(fset *token.FileSet, funcDecl *ast.FuncDecl, impMeta map[string]int) (funcObjParamMap, depObjMap, error) {
 	var params map[string]*obj.FuncObjParam
 	if len(funcDecl.Type.Params.List) > 0 {
 		params = make(map[string]*obj.FuncObjParam)
