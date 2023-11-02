98a6537afb167bc3c4eaa7d36d502df6be328103
g10z3r <20g10z3r@gmail.com>
2023-11-02 19:49:01 +0100

    off some code (for now)

--- internal/domain/service/anthill/config/config.go
+++ internal/domain/service/anthill/config/config.go
@@ -1,10 +1,8 @@
 package config
 
-import "github.com/g10z3r/archx/internal/domain/service/anthill/analyzer"
-
-type Config struct {
-	Analysis   analyzer.AnalyzerMapOld
-	ModuleName string
-	RootDir    string
-	TargetDir  string
-}
+// type Config struct {
+// 	Analysis   analyzer.AnalyzerMapOld
+// 	ModuleName string
+// 	RootDir    string
+// 	TargetDir  string
+// }
--- internal/domain/service/anthill/event/pkg_formed.go
+++ internal/domain/service/anthill/event/pkg_formed.go
@@ -2,6 +2,10 @@ package event
 
 import "github.com/g10z3r/archx/internal/domain/service/anthill/obj"
 
+// e.eventCh <- &event.PackageFormedEvent{
+// 	Package: e.parsePkg(fset, pkgAst, targetDir, info.ModuleName),
+// }
+
 type PackageFormedEvent struct {
 	Package *obj.PackageObj
 }
